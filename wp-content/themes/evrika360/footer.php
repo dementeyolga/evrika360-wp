87ae3be6598d1084f0bd0d3dc83887984da3a3ba
dementeyolga <dementeyolya@gmail.com>
2024-10-30 11:06:46 +0300

    feat: complete footer desktop layout

--- wp-content/themes/evrika360/footer.php
+++ wp-content/themes/evrika360/footer.php
@@ -1,35 +1,39 @@
 <?php wp_footer(); ?>
 
-<footer class="wrapper mt-[64px] pb-10 space-y-8">
-  <div>
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/evrika-logo-footer.png" alt="">
-  </div>
+<footer class="wrapper mt-[64px] pb-10 space-y-8 lg:space-y-10">
+  <div class="flex flex-col gap:8 lg:flex-row lg:justify-between lg:items-center">
+    <div>
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/evrika-logo-footer.png" alt="">
+    </div>
 
-  <div class="space-y-1">
-    <p class="small-text">
-      Пн-Пт 10:00 — 18:00
-    </p>
+    <div class="space-y-1">
+      <p class="small-text">
+        Пн-Пт 10:00 — 18:00
+      </p>
 
-    <div class="flex gap-4 font-bold">
-      <p>+7 495 662-18-73</p>
-      <p> +375 29 388-55-12</p>
+      <div class="flex gap-4 lg:gap-8 font-bold">
+        <p>+7 495 662-18-73</p>
+        <p> +375 29 388-55-12</p>
+      </div>
+    </div>
+
+    <div class="flex gap-8 lg:gap-4">
+      <button class="mx-0 xs:shrink xs:grow-0 grow btn dark">Заказать звонок</button>
+
+      <div class="grow-0 flex gap-2 items-center justify-end">
+        <a>
+          <img class="lg:hidden" src="<?php echo get_template_directory_uri() ?>/assets/images/telegram-icon.png" alt="">
+          <img class="hidden lg:block" src="<?php echo get_template_directory_uri() ?>/assets/images/telegram-icon-desktop.png" alt="">
+        </a>
+        <a>
+          <img class="lg:hidden" src="<?php echo get_template_directory_uri() ?>/assets/images/whatsapp-icon.png" alt="">
+          <img class="hidden lg:block" src="<?php echo get_template_directory_uri() ?>/assets/images/whatsapp-icon-desktop.png" alt="">
+        </a>
+      </div>
     </div>
   </div>
 
-  <div class="flex gap-8">
-    <button class="mx-0 xs:shrink xs:grow-0 grow btn dark">Заказать звонок</button>
-
-    <div class="grow-0 flex gap-2 items-center justify-end">
-      <a>
-        <img src="<?php echo get_template_directory_uri() ?>/assets/images/telegram-icon.png" alt="">
-      </a>
-      <a>
-        <img src="<?php echo get_template_directory_uri() ?>/assets/images/whatsapp-icon.png" alt="">
-      </a>
-    </div>
-  </div>
-
-  <div class="py-4 space-y-4 *:small-text border-t border-t-grey-100">
+  <div class="py-4 flex flex-col gap-y-4 lg:flex-row lg:justify-between *:small-text border-t border-t-grey-100">
     <p class="text-grey-400">
       ООО «ИТ-Глобал» <br>
       223070, Минская область, Минский район, <br>
